7807676d9c18e7b627280bcd7f35a3787f64ec3f
Denys Plietnikov <givisimo@gmail.com>
2019-07-13 01:09:48 +0300

    css update

--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@
 
 import getGeoPosition from './getGeoPosition';
 import fetchWeather from './fetchWeather';
+import './css/reset.css';
 import './css/styles.css';
-import alertOn from './Pnotify'
 
-
+import PNotify from 'pnotify/dist/es/PNotify.js';
+import PNotifyButtons from 'pnotify/dist/es/PNotifyButtons.js';
+PNotify.defaults.delay -= 6000;
+const optionsPNotify = {
+  text: "Нет прав доступа к геопозиции, используйте поиск по имени города!",
+  context: document.getElementById('search-form'),
+  stack: {
+    'dir1': 'down',
+    'firstpos1': 25
+  }
+};
 
 const weatherWrapper = document.querySelector('#weather');
 const locationValue = document.querySelector('span[data-field="location"]');
@@ -19,9 +29,9 @@ const searchForm = document.querySelector('#search-form');
 function markup(weather) {
   weatherWrapper.classList.remove('is-hidden');
   locationValue.textContent = weather.location.name;
-  tempValue.textContent = weather.current.temp_c;
-  humidityValue.textContent = weather.current.humidity;
-  windValue.textContent = weather.current.wind_kph;
+  tempValue.textContent = `${weather.current.temp_c}\xB0`;
+  humidityValue.textContent = `${weather.current.humidity}\u0025`;
+  windValue.textContent = `${weather.current.wind_kph} kph`;
   conditionsValue.textContent = weather.current.condition.text;
   imgOfConditionValue.setAttribute('src', weather.current.condition.icon);
 }
@@ -42,6 +52,6 @@ getGeoPosition()
     });
   })
   .catch(() => {
-    alertOn;
+    PNotify.info(optionsPNotify);
     searchForm.addEventListener('submit', handleClick);
   });
