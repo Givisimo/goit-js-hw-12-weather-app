e175efb9dbb551b5e6ed23edb94bb8e5b35dfd4f
Denys Plietnikov <givisimo@gmail.com>
2019-07-13 01:13:31 +0300

    fetchWeather.js change

--- src/fetchWeather.js
+++ src/fetchWeather.js
@@ -4,6 +4,6 @@ export default fetchWeather;
 
 function fetchWeather(query) {
   return fetch(
-    `http://api.apixu.com/v1//current.json?key=2eb7d60abecf4583bb3222242191007&q=${query}`,
+    `https://api.apixu.com/v1//current.json?key=2eb7d60abecf4583bb3222242191007&q=${query}`,
   ).then(response => response.json());
 }
